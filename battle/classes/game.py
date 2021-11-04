0bed1749f319e5071e6e98ff585e9e051dc25642
ilovemacross <renanmachadopinho@gmail.com>
2021-11-04 09:20:20 -0300

    here

--- battle/classes/game.py
+++ battle/classes/game.py
@@ -25,4 +25,44 @@ class Person:
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
 
-    def 
+    def generate_spell_damage(self,i):
+        mgl= self.magic[i]["dmg"]-5
+        mgh= self.magic[i]["dmg"]+5
+
+        return random.randrange(mgl, mgh)
+
+    def take_damage(self,dmg):
+        self.hp-= dmg 
+        if self.hp <0:
+            self.hp=0 
+            return self.hp
+        
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def reduce_mp(self,cost):
+        self.mp -= cost
+
+    def get_spell_name(self):
+        return self.magic[i]["name"]
+
+    def get_spell_mp_cost(self,i):
+        return self.magic[i]["cost"]
+
+    def choose_action(self):
+        i= 1
+        print(bcolors.OKBLUE + bcolors.BOLD + "Actions"+ bcolors.ENDC)
+        
+        for item in self.actions:
+            print(str(i)+ ":", item)
+            i +=1
+
+    def choose_spell(self):
+        i= 1
+        print(bcolors.OKBLUE + bcolors.BOLD + "Magic" +bcolors.ENDC)
+        for spell in self.magic:
+            print(str(i)+ ":", spell["name"], "(cost)",str(spell["mp"]) + ")")
+            i+=1
--- battle/main.py
+++ battle/main.py
@@ -1,4 +1,24 @@
 from classes.game import Person, bcolors
 
-magic=[{"name": }]
+magic=[ {"name":"Fire","cost":10,"dmg":60 },
+        {"name":"Thunder","cost":10,"dmg":60 },
+        {"name":"Blizzard","cost":10,"dmg":60 }
+]
+
+
+
 player = Person(460, 65,60,34,magic)
+enemy= Person(1200,65,45,25,magic)
+
+running= True
+i= 0
+
+
+print(bcolors.FAIL+ bcolors.BOLD+"AN ENEMY ATTACKS!"+ bcolors.ENDC)
+
+while running:
+    print("================================================================")
+    player.choose_action()
+    choice= input("Choose action: ")
+    print("You choose ",choice)
+    running= False
